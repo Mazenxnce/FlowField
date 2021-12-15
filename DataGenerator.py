b92671edab050c22769c57ee30d59454b7a5f2cd
Zain Moffatt <zain@MazeMBP.local>
2021-12-15 18:27:56 +0800

    Integrated with InfluxDB (Write)

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,9 +1,18 @@
+import datetime
 import random
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import matplotlib.pyplot as plt
+
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "Eo8wFg4vDe4nsVMppA-gtsvuMjxCJiYIVjZgb-Ui3UTQ_LWPFNb5a-36zcnfckrBrIVVzl82xzbgOlvfWr_JqQ=="
+org = "Maze"
+bucket = "AnalyticalData"
+client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
+
 
 # Initialize and create dataframe
 headers = ['Particle_X', 'Particle_Y', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'Velocity_X', 'Velocity_Y']
@@ -18,12 +27,11 @@ particle = 1
 n = 6
 # X represents the position of the particle (must be within the boundary)
 
-for k in tqdm(range(1000000)):
+for k in tqdm(range(10000)):
     velocity = []
     x = []
 
-
-    class Point:
+    class Origin:
         def __init__(self, x, y):
             self.x = x
             self.y = y
@@ -31,14 +39,12 @@ for k in tqdm(range(1000000)):
         def __str__(self):
             return str((self.x, self.y))
 
-
     class Circle:
         def __init__(self, origin, radius):
             self.origin = origin
             self.radius = radius
 
-
-    origin = Point(0, 0)
+    origin = Origin(0, 0)
     radius = w
     circle = Circle(origin, radius)
 
@@ -48,6 +54,10 @@ for k in tqdm(range(1000000)):
         x.append(r * np.cos(p))
         x.append(r * np.sin(p))
 
+        # Append to db
+        ParticleX = r * np.cos(p)
+        ParticleY = r * np.sin(p)
+
     ParticleLocation = np.array(x)
     row.append(x)
 
@@ -55,11 +65,16 @@ for k in tqdm(range(1000000)):
     Q = []
     Q = StandardScaler().fit_transform(np.random.rand(6, 1, ))
     Q = Q.ravel().tolist()
-    # for i in range(n - 1):
-    #     Q.append(random.uniform(-1.0, 1.0))
-    #     # Remember for mass conservation
-    # Q.append(0 - sum(Q))
     row.append(Q)
+
+    # Append to db
+    q_1 = Q[0]
+    q_2 = Q[1]
+    q_3 = Q[2]
+    q_4 = Q[3]
+    q_5 = Q[4]
+    q_6 = Q[5]
+
 
     # R represents the vector that contains elements that represent the coordinates of the ith source/sink
     R = []
@@ -76,12 +91,28 @@ for k in tqdm(range(1000000)):
     particle_vel = (sum(velocity))
     row.append(particle_vel.tolist())
 
+    # Append to db
+    vel = particle_vel.tolist()
+    VelX = vel[0]
+    VelY = vel[1]
+
     data_row = (sum(row, []))
     dataset.loc[len(dataset)] = data_row
     data_row.clear()
     row.clear()
+   
+    # Write to DB
+    datapoint = Point("AnalyticalSolution").tag("Data Description", "Testing Data").field("Particle_X",ParticleX).field("Particle_Y",ParticleY).field("Q1",q_1).field("Q2",q_2).field("Q3",q_3).field("Q4",q_4).field("Q5",q_5).field("Q6",q_6).field("Velocity_X",VelX).field("Velocity_Y",VelY).time(datetime.datetime.utcnow().isoformat())
 
-# print(dataset)
-# dataset.boxplot()
-# plt.show()
+    with client.write_api(write_options=WriteOptions(batch_size=10000,
+                                                    flush_interval=10,
+                                                    jitter_interval=0,
+                                                    retry_interval=5_000,
+                                                    max_retries=5,
+                                                    max_retry_delay=30_000,
+                                                    exponential_base=2)) as _write_client:
+                                                    _write_client.write(bucket, org, datapoint) 
+
+
 dataset.to_csv("2D_Hele_Shaw_Data (test).csv", index=False)
+
